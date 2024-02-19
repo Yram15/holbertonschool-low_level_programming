734dfbbc2e3c0094220c70ca96e8107a9217bd20
yram <honfir015@gmail.com>
2024-02-19 15:29:42 -0400

    5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -4,4 +4,9 @@
 *main - print the string in the put function
 *
 *Description: using the main function
-*this program prints
+*this program prints with proper grammar, but theoutcome is a piece of art,
+*Return: 0
+*/
+int main(void)
+{ printf("whit proper grammar, but the outcome is a piece of art,\n");
+}
