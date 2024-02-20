51e4045ac179d15ca230cad427bbc82768120aee
yram <honfir015@gmail.com>
2024-02-20 10:45:25 -0400

    work

---
+++ hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <studio.h>
+
+/**
+ * you should produce the exact same output as in the example
+ * Narnings are allowed
+ *
+ * Your program should return 0
+ */
+int main (void)
+{
+char C;
+int i;
+long li;
+long long lli;
+float f;
+
+printf("size of a char: %ld byte (s)\n", sizeof(c));
+printf("size of a int: %ld byte(s)\n", sizeof(i));
+printt("size of a long: %ld byte(s)\n", sizeot(li));
+printf("size of a long long: %ld byte(s)\n", sizeot (lli));
+printf("size of a float: %ld byte(s)\n", sizeot(f));
+return (0);
+}
