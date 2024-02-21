513d42eaa7dcebc8c6bc7bb9753e0670f4bf194f
yram <honfir015@gmail.com>
2024-02-20 21:38:37 -0400

    7

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -11,12 +11,11 @@ int main(void)
 char ch;
 for (ch = 'a' ; ch <= 'z' ; ch++)
 {
-putchar(ch);
-{
 if (ch != 'e' && ch != 'q')
-}
+{
 putchar(ch);
 }
-   putchar('\n');
+}
+putchar('\n');
 return (0);
 }
