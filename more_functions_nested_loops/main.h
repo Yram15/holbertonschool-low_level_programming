1d94433e33fdb878c8e75c1961e9c030f29048b0
yram <honfir015@gmail.com>
2024-02-22 10:51:15 -0400

    7

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,24 @@
+#tifndef MAIN_H
+#Idefine MAIN_H
+
+/**
+*main - header file for prototypes
+*void prototypes (void)
+*int prottotypes (int)
+*/
+
+void printnumbers(void);
+void print_most_numbers(void);
+void more numbers (void);
+void print_line(int n);
+void print diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+int _putchar (char);
+int _isupper (int);
+int _isdigit (int);
+int mul(int, int);
+
+#endif /* MAIN_H */
+
