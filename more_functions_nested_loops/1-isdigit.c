611154802e876e3dbcb2f8c741c863d500059261
yram <honfir015@gmail.com>
2024-02-22 13:29:03 -0400

    7

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*_isupper - checks if a character is uppercase or not
+*@c: character to be tesed
+*Return: (1) for true (0) false
+*
+*/
+
+int _isupper(int c)
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+else
+}
+}
