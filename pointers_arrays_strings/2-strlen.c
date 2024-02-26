c17960d8075a022ab13ab79dbc5a5d883446e67e
yram <honfir015@gmail.com>
2024-02-26 10:58:27 -0400

    5

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+int __strlen(char *s);
+{
+int login = 0;
+while (*s != '\0')
+{
+login++;
+S++;
+}
+return (longi);
+}
