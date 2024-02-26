d751f77388782154026a1c8babbdc5a7cece9c24
yram <honfir015@gmail.com>
2024-02-26 13:25:48 -0400

    5

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 /**
- * _strlen - returns the length of a string 
- * @s: string 
+ * _strlen - returns the length of a string
+ * @s: string
  * Return: length
  */
 int __strlen(char *s);
