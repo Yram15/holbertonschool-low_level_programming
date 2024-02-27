b4f165634a166af64f606d5ae25cf6f711b3d45a
yram <honfir015@gmail.com>
2024-02-27 14:08:29 -0400

    0

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,40 +1,25 @@
 #include "main.h"
-
 /**
-*  _strcmp - Function which compare two strings and
-*@s1: first string
-*@s2:second string
-*Return:
-*returns zero if s1 == s2
-*returns negative number if s1 < s2
-*returns positive number if s1 > s2
+* _strncpy - copy a string
+* @dest: input vale
+* @src: input vale    
+* @n: input vale
+* Return: dest
 */
+char *_strcat(char *dest, char *src, int n)
+{
+int j;
 
-int _strcmp(char *s1, char *s2)
+j = 0;
+while (j < n && src[j] != '\0'
 {
-int i = 0, diff = 0;
-
-while (1)
-{
-if (s1[i] == '\0' && s2[i] == '\0')
-break;
-else if (s1[i] == '\0')
-{
-diff = s2[i];
-break;
+dest[j] = src[j];
+j++;
 }
-else if (s2[i] == '\0')
+while (j < n)
 {
-diff = s1[i];
-break;
+dest[j] = '\0';
+j++;
 }
-else if (s1[i] != s2[i])
-{
-diff = s1[i] - s2[i];
-break;
-}
-else
-i++;
-}
-return (diff);
+return (dest);
 }
