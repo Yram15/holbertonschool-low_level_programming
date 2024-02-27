883f6306979d25e0470c23d8dcc9aa47a75c8564
yram <honfir015@gmail.com>
2024-02-27 13:31:20 -0400

    0

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,25 +1,23 @@
 #include "main.h"
 /**
-* _strcat - concatenates two strings
-* @dest: input vale
-* @src: input vale    
-* @n: input valechar *_strcat(char *dest, char *src, int n)
-{
-int i;
-int j;
+ *_strncat - concatenate two strings but add inputted number of bytes
+ *@dest: string to be appended upon
+ *@src: string to be completed at end of dest
+ *@n:integer parameter to compare index to
+ *Return: returns new concatenated string
+ */
 
-i = 0;
-while (dest[i] != '\0')
+char *_strncat(char *dest, char *src, int n)
 {
-i++;
-}
-j = 0;
-while (src[i] != '\0')
-{
-dest[i] = src[j];
-i++;
-j++;
-}
-dest[i] = '\0'; 
-return (dest);
+
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+
 }
