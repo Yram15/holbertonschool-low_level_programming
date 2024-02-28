b0f66b9bfa70dc0998055d0d1aaaea69368502f0
yram <honfir015@gmail.com>
2024-02-28 10:03:58 -0400

    0

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -8,18 +8,13 @@
 */
 char *_strcat(char *dest, char *src, int n)
 {
-int j;
+        int i;
 
-j = 0;
-while (j < n && src[j] != '\0'
-{
-dest[j] = src[j];
-j++;
-}
-while (j < n)
-{
-dest[j] = '\0';
-j++;
-}
-return (dest);
+        for (i = 0; i < n && src[i] != '\0'; i++)
+                dest[i] = src[i];
+
+        for ( ; i < n; i++)
+                dest[i] = '\0';
+
+        return (dest);
 }
