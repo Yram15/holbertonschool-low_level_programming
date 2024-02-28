661e88c59027893ba6fa123649a1f8bd851333cd
yram <honfir015@gmail.com>
2024-02-28 13:35:52 -0400

    0

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,36 +1,23 @@
 #include "main.h"
-/**
- *cap_string - capitalizes every first letter of a word in a string.
- *separators of words are:  space, tabulation,
- * new line, ,, ;, ., !, ?, ", (, ), {, and }.
- *@s: pointer to string.
- *
- *Return: pointer to s.
- */
-char *cap_string(char *s)
-{
-int count;
+#include <ctype.h>
 
-/*  scan through string */
-]count = 0;
-while (s[count] != '\0')
-{/* if next character after count is a char , capitalise it */
-if (s[0] >= 97 && s[0] <= 122)
+/**
+ * cap_string - capitalizes the first letter of each word in a string
+ * @str: the string to capitalize
+ *
+ * Return: a pointer to the capitalized string
+ */
+char *cap_string(char *str)
 {
-s[0] = s[0] - 32;
-}
-if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
-    || s[count] == ',' || s[count] == ';' || s[count] == '.'
-    || s[count] == '.' || s[count] == '!' || s[count] == '?'
-    || s[count] == '"' || s[count] == '(' || s[count] == ')'
-    || s[count] == '{' || s[count] == '}')
-{
-if (s[count + 1] >= 97 && s[count + 1] <= 122)
-{
-s[count + 1] = s[count + 1] - 32;
-}
-}
-count++;
-}
-return (s);
+    char *ptr;
+
+    for (ptr = str; *ptr; ptr++)
+    {
+        if (isspace((unsigned char)*(ptr - 1)) || !isalpha((unsigned char)*(ptr - 1)))
+        {
+            if (islower((unsigned char)*ptr))
+                *ptr = toupper((unsigned char)*ptr);
+        }
+    }
+    return (str);
 }
