8e78dd503cf4712ad9213281c02319c74b3e339e
yram <honfir015@gmail.com>
2024-03-07 10:39:42 -0400

    7

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -1,21 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-
 int _putchar(char c);
-/* Task 0 */
 void *malloc_checked(unsigned int b);
-/* Task 1 */
 char *string_nconcat(char *s1, char *s2, unsigned int n);
-/* Task 2 */
 void *_calloc(unsigned int nmemb, unsigned int size);
-/* Task 3 */
 int *array_range(int min, int max);
-/* Task 4 */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-
-#endif /* MAIN_H */
