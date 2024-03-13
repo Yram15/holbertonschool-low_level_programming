f623bd070730dc23ec7f159baf86a2e73694ab5b
yram <honfir015@gmail.com>
2024-03-13 14:17:15 -0400

    7

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -8,7 +8,7 @@
  * @owner : owner's dog
  * Return : newdog
  */
-dog_t *4-new_dog(char *name, float age, char *owner)
+dog_t *new_dog(char *name, float age, char *owner)
 {
   int i = 0, j = 0, k;
   dog_t *doge;
