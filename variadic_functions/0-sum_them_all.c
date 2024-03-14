120f54190aa9745f51a74a570cfd2a1e1e48ff1b
yram <honfir015@gmail.com>
2024-03-14 11:18:03 -0400

    7

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,4 +1,5 @@
 #include "variadic_functions.h"
+
 /**
  * sum_them_all - sum all parameter passd to the function
  * @n: number of argument to be passed to sum
@@ -10,13 +11,17 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	va_list holder;
 
-	if (n == 0)
-		return (0);
-
 	va_start(holder, n);
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(holder, const unsigned int);
+		if (n == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			sum += va_arg(holder, const unsigned int);
+		}
 	}
 	va_end(holder);
 
