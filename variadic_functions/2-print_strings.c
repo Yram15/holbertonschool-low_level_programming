b85fed4a97e4c544d13e450695dfb2595e0954a3
yram <honfir015@gmail.com>
2024-03-14 14:10:54 -0400

    7

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,31 +1,37 @@
-#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
 /**
-* print_strings - print strings passed to a funtion as parameter
-* @separator: separator between two strings
-* @n: number of strings to print
-*/
-void print_strings(const char *separator, const unsigned int n, ...);
+  *print_strings - prints strings followed by a new line.
+  *@separator: string to be printed between strings.
+  *@n: number of strings.
+  *Return: void.
+  */
+
+void print_strings(const char *separator, const unsigned int n, ...)
 {
-unsigned int i;
-char *str;
-va_list ptr_str;
-va_start(ptr_str, n);
-for (i = 0; i < n; i++)
-{
-str = va_arg(ptr_str, char *);
-if (str == NULL)
-{
-printf("(null)");
-}
-else
-{
-printf("%s", str);
-}
-if (i != (n - 1) && separator != NULL)
-{
-printf("%s", separator);
-}
-}
-printf("\n");
-va_end(ptr_str);
+	unsigned int i;
+	char *s;
+	va_list strings;
+
+	if (separator == NULL)
+		separator = "";
+
+	va_start(strings, n);
+
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(strings, char *);
+
+		if (s == NULL)
+			s = "(nil)";
+		printf("%s", s);
+		if (i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(strings);
 }
