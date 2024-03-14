755c8262559c7814db68928dd9d1ee7002e6ab92
yram <honfir015@gmail.com>
2024-03-14 12:22:46 -0400

    7

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,22 +1,27 @@
 #include "variadic_functions.h"
+
 /**
  * print_numbers - print integers
- * @seperator: string to be printed btwn numbers
+ * @separator: string to be printed btwn numbers
  * @n: number of ints to print
  * Return: void
  */
-void print_numbers(const char *separator, const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i, arr;
-	va_list ptr;
-	va_start(ptr, n);
-	fot (i = 0; i < n; i++)
-	{
-		arr = va_arg(ptr, const unsigned int);
-		ptintf("%d", arr);
-		if (i != (n - 1) && separator != NULL)
-			ptintf("%s", separator);
-	}
-	ptintf("\n");
-	va_end(ptr);
+unsigned int i, arr;
+va_list ptr;
+
+va_start(ptr, n);
+
+for (i = 0; i < n; i++)
+{
+arr = va_arg(ptr, unsigned int);
+printf("%d", arr);
+
+if (i != (n - 1) && separator != NULL)
+printf("%s", separator);
+}
+
+printf("\n");
+va_end(ptr);
 }
