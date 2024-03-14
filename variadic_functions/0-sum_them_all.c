612c748f40c42200e77ee864e3dc9ad52a22c6f8
yram <honfir015@gmail.com>
2024-03-14 11:08:27 -0400

    7

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -6,21 +6,19 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-unsigned int sum = 0;
-unsigned int i;
-va_list holder;
-va_start(holder, n);
-for (i = 0; i < n; i++)
-{
-if (n == 0)
-{
-return (0);
-}
-else
-{
-sum += va_arg(holder, const unsigned int);
-}
-}
-va_end(holder);
-return (sum);
+	unsigned int sum = 0;
+	unsigned int i;
+	va_list holder;
+
+	if (n == 0)
+		return (0);
+
+	va_start(holder, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(holder, const unsigned int);
+	}
+	va_end(holder);
+
+	return (sum);
 }
