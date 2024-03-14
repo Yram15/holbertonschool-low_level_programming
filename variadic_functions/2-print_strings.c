b333ca2c163a4500e696b69cd0365baa5f465826
yram <honfir015@gmail.com>
2024-03-14 13:12:35 -0400

    7

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+/**
+* print_strings - print strings passed to a funtion as parameter
+* @separator: separator between two strings
+* @n: number of strings to print
+*/
+void print_strings(const char *separator, const unsigned int n, ...);
+{
+unsigned int i:
+char *str;
+va_list ptr_str;
+va_start(ptr_str, n);
+for (i = 0; i < n; i++)
+{
+str = va_arg(ptr_str, char *);
+if (str == NULL)
+{
+printf("(nill)");
+}
+else
+{
+printf("%s", str);
+}
+if (i != (n - 1) && separator != NULL)
+printf("%S", separator);
+}
+print("\n");
+va_end(ptr_str);
+}
